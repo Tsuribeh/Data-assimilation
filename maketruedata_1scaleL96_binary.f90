f01477d1ea5cd6c827a2cb28f713044aeb9a6e0e
BroadFisher <ht.fisher@protonmail.com>
2025-05-24 11:40:54 +0900

    初期値を乱数で与えるようにした

--- maketruedata_1scaleL96_binary.f90
+++ maketruedata_1scaleL96_binary.f90
@@ -8,9 +8,9 @@ program maketruedata_binary
     real(8), parameter :: c = 4.0d0
 
     ! Simulation parameters
-    integer, parameter :: total_inits = 1000  ! アトラクタ上のデータの数
+    ! integer, parameter :: total_inits = 1000  ! アトラクタ上のデータの数
     integer, parameter :: data_span = 5
-    integer, parameter :: n_timesteps = data_span*4*365*100
+    integer, parameter :: n_timesteps = data_span*4*365*10000
     integer, parameter :: n_savedata = data_span*4*365
 
     integer, parameter :: kmax = 40
@@ -19,10 +19,13 @@ program maketruedata_binary
     real(8), parameter :: dt = 1.0d-2
 
     ! Variables for storing x1, x2 data and selected random initial condition
-    real(8) :: x1_true(kmax), x2_true(jmax*kmax)
-    real(8) :: random_num
-    integer :: random_index
-    integer :: i, j, n, iostat, jmaxkmax
+    real(8) :: x1_true(kmax)
+    ! real(8) :: x2_true(jmax*kmax)
+
+    ! real(8) :: random_num
+    ! integer :: random_index
+    ! integer :: i, j, iostat, jmaxkmax
+    integer :: n
     integer :: Ampli, Freq
 
     ! Initialize state variables
@@ -30,45 +33,48 @@ program maketruedata_binary
 
     ! File units
     integer, parameter :: iunit_x1 = 12
-    integer, parameter :: iunit_x2 = 22
+    ! integer, parameter :: iunit_x2 = 22
     integer, parameter :: iunit_t = 32
     integer, parameter :: iunit_x1t = 14
-    integer, parameter :: iunit_x2t = 24
+    ! integer, parameter :: iunit_x2t = 24
 
-    ! Open binary files for input and output
-    open(iunit_x1, file="1scale_x_initial_km40_F8.bin", form='unformatted', access='stream', action="read", iostat=iostat)
-    if (iostat /= 0) then
-        print *, "Error: Could not open x1 binary file."
-        stop
-    end if
+    ! ! Open binary files for input and output
+    ! open(iunit_x1, file="1scale_x_initial_km40_F8.bin", form='unformatted', access='stream', action="read", iostat=iostat)
+    ! if (iostat /= 0) then
+    !     print *, "Error: Could not open x1 binary file."
+    !     stop
+    ! end if
 
     open(iunit_x1t, file="1scalePlusSinVarious_xtrue_timeseries_km40_F8_50span.bin", &
     & form='unformatted', access='stream', status="replace")
 
-    open(iunit_t, file="1scale_t_truedata_fort_50span.bin", form='unformatted', access='stream', status="replace")
+    open(iunit_t, file="1scalePlusSinVarious_t_truedata_fort_50span.bin", form='unformatted', access='stream', status="replace")
 
-    do Ampli = -11, 3
-        do Freq = -11,3
-            ! Initialize random seed and select a random index
+    do Ampli = -4, 4
+        do Freq = -5,5
+            !! Initialize random seed and select a random index!!
             call random_seed()
-            call random_number(random_num)
-            random_index = int(random_num * total_inits) + 1  ! 1 〜 total_inits
+            ! call random_number(random_num)
+            ! random_index = int(random_num * total_inits) + 1  ! 1 〜 total_inits
 
-            ! Rewind the file to the beginning before skipping
-            rewind(iunit_x1)
+            ! ! Rewind the file to the beginning before skipping!!
+            ! rewind(iunit_x1)
 
-            ! Move the file pointers to the random index
-            call skip_records(iunit_x1, random_index - 1, kmax)
+            ! ! Move the file pointers to the random index !!
+            ! call skip_records(iunit_x1, random_index - 1, kmax)
 
-            ! Read the selected initial condition
-            read(iunit_x1) x1_true  ! アトラクタ上の点 x1
+            ! ! Read the selected initial condition !!
+            ! read(iunit_x1) x1_true  ! アトラクタ上の点 x1
 
-            ! Output the selected random initial condition for verification
-            print *, "Random initial condition (x1):", x1_true
-            print *, "Random index:", random_index
+            ! ! Output the selected random initial condition for verification !!
+            ! print *, "Random initial condition (x1):", x1_true
+            ! print *, "Random index:", random_index
 
-            ! Time integration
+
+            ! Time integration !
             t = 0.0d0
+            call initialize(x1_true)
+
             do n = 1, n_timesteps
 
                 ! Runge-Kutta 4th order integration step
@@ -92,6 +98,16 @@ program maketruedata_binary
     close(iunit_t)
 
 contains
+
+
+    ! Function to initialize x1 and x2
+    subroutine initialize(x1)
+        real(8), intent(out) :: x1(kmax)
+
+        ! Initialize x1 and x2 with random values
+        x1 = stdnorm_err(kmax)
+    end subroutine initialize
+
 
     ! Subroutine to skip a given number of records in a binary file
     subroutine skip_records(unit, num_records, record_size)
@@ -143,4 +159,19 @@ contains
         end do
     end subroutine model_nature_1
 
+
+    function stdnorm_err(n) result(z1)
+        implicit none
+        integer, intent(in) :: n
+        real(8) :: z1(n)
+        real(8) :: u1, u2
+        integer :: i
+
+        ! Box-Muller method for generating normal random numbers
+        do i = 1, n
+            call random_number(u1)
+            call random_number(u2)
+            z1(i) = sqrt(-2.0d0 * log(u1)) * cos(2.0d0 * acos(-1.0d0) * u2)
+        end do
+    end function stdnorm_err
 end program maketruedata_binary
